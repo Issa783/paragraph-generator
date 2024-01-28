2eb4051596bc7a72bdcb41f5a4df7e1e09bb8d7d
John Mutua <mutubenedict920@gmail.com>
2024-01-28 10:54:12 +0300

    Added paragraph-generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styles from "./style.module.css"
+import Data from "./data"
+
 
 function App() {
+  const[count,setCount] = useState(0)
+  const[text,setText] = useState([])
+  const handleSubmit = (e) => {
+     e.preventDefault()
+     let amount = parseInt(count)
+     if(count <= 0){
+      amount = 1
+     }
+     if (count > 8){
+      amount = 8
+     }
+     setText(Data.slice(0,amount))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <section className={styles.sectioncenter}>
+      <h3>Tired of lorem ipsum?</h3>
+      <form className={styles.loremform} onSubmit={handleSubmit}>
+         <label htmlFor='amount'>Paragraphs :</label>
+         <input type='number' id='amount' value={count} onChange={(e) => {
+          setCount(e.target.value)}}/>
+          <button className={styles.btn}>Generate</button>
+      </form>
+      <article className={styles.loremtext}></article>
+      {text.map((item,index)=> {
+        return <p key={index}>{item}</p>
+      })}
+     </section>
   );
 }
 
